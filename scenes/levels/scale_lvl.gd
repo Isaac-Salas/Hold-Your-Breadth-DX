7f99d84604b4a9bdbecaf2107fd77983f9a7d199
alexcavadora <49330757+alexcavadora@users.noreply.github.com>
2024-08-25 14:46:31 -0600

    light for scale

--- scenes/levels/scale_lvl.gd
+++ scenes/levels/scale_lvl.gd
@@ -5,3 +5,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	transition.ap.play("Opening")
+
+func _unhandled_input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("Reset"):
+		get_tree().reload_current_scene()
