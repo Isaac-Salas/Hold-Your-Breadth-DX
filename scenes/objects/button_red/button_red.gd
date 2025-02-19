7a07a6b4b7dbdd10c63e53bafb7e95027cd53eaf
Alejandro Alonso Sánchez <a.alonsosanchez@ugto.mx>
2025-02-18 23:07:22 -0600

    quiet quitting

--- scenes/objects/button_red/button_red.gd
+++ scenes/objects/button_red/button_red.gd
@@ -1,6 +1,6 @@
 extends Area2D
 @onready var anim_flag: AnimatedSprite2D = $anim_flag
-@onready var light: Sprite2D = $PointLight2D
+@onready var light = $PointLight2D
 @onready var anim: AnimatedSprite2D = $anim
 signal pressed(state, body)
 var state = false
@@ -12,19 +12,23 @@ func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("button_actionable"):
 		n_inside += 1
 		anim_flag.play("ON")
-		if  not state:
+		if not state:
 			state = true
 			anim.play("pressed")
 			light.show()
+			await anim.animation_finished
 			emit_signal("pressed", state, body)
 
 
 func _on_body_exited(body: Node2D) -> void:
 	if body.is_in_group("button_actionable"):
 		n_inside -= 1
-		if state and n_inside == 0:
+		print(n_inside)
+		if n_inside == 0:
+			anim.play("depresssed")
 			anim_flag.play("OFF")
 			state = false
 			light.hide()
+			await anim.animation_finished
 			emit_signal("pressed", state, body)
-			anim.play_backwards("pressed")
+			
