d1c1b071d801bc1e84746cbc4059aa1541dde014
Isaac Salas <iscprepa@gmail.com>
2025-02-20 12:10:12 -0600

    Options Screen
    Fully featured options menu

---
+++ scenes/shaders/options_menu.gd
@@ -0,0 +1,65 @@
+extends CanvasLayer
+@export var Enviroment : WorldEnvironment
+@onready var brightnes = $Brightnes
+@onready var contrast = $Contrast
+@onready var saturation = $Saturation
+@onready var toggle : int = 0
+
+@onready var fullscreen = $Fullscreen
+@onready var crt = $CRT
+@onready var exclusive = $Exclusive
+@export var CRTShader : ColorRect
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	brightnes.value = Enviroment.environment.adjustment_brightness
+	contrast.value = Enviroment.environment.adjustment_contrast
+	saturation.value = Enviroment.environment.adjustment_saturation
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_just_pressed("Esc"):
+		if toggle < 1:
+			visible = true
+			toggle += 1
+		else:
+			visible = false
+			toggle = 0
+		print(toggle)
+		
+	
+	Enviroment.environment.adjustment_brightness = brightnes.value
+	Enviroment.environment.adjustment_contrast = contrast.value
+	Enviroment.environment.adjustment_saturation = saturation.value
+	
+
+func _on_reset_pressed():
+	brightnes.value = 1
+	contrast.value = 1
+	saturation.value = 1
+
+
+func _on_fullscreen_toggled(toggled_on):
+	if fullscreen.button_pressed == true:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+		exclusive.visible = true
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		exclusive.visible = true
+		exclusive.button_pressed = false
+
+
+func _on_crt_toggled(toggled_on):
+	if crt.button_pressed == true:
+		CRTShader.visible = true
+	else:
+		CRTShader.visible = false
+
+
+func _on_exclusive_toggled(toggled_on):
+	if exclusive.button_pressed == true:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+		
