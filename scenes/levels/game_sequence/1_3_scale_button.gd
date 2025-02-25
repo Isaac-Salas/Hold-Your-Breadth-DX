feee1e4cf5764ac0645a3b470c34f2af6be6c6f0
Isaac Salas <iscprepa@gmail.com>
2025-02-25 14:57:57 -0600

    Bouta add some bugs and changes

--- scenes/levels/game_sequence/1_3_scale_button.gd
+++ scenes/levels/game_sequence/1_3_scale_button.gd
@@ -1,16 +1,11 @@
 extends Node2D
 
 @onready var transition: Node2D = $Transition
-@export var ZoneNumber : int
-@export var LevelNumber : int
 
 func _ready():
-	var LevelSave : String = "Level" + str(ZoneNumber) + "_" + str(LevelNumber)
-	Manager.setter(LevelSave,true)
-	Manager.save_game()
+	Manager.Level1_3 = true
+	Manager.save()
 
-
-	
 func _unhandled_input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("Reset"):
 		get_tree().reload_current_scene()
