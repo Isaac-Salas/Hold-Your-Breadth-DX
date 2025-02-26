60183608d1091a7da6b75cc1810fc1d5f1e6ad2b
Isaac Salas <iscprepa@gmail.com>
2025-02-26 17:14:49 -0600

    Changes?

--- scenes/objects/Wall_Breakable/wall_broken.gd
+++ scenes/objects/Wall_Breakable/wall_broken.gd
@@ -8,34 +8,36 @@ var counter : float = 0
 var children
 var startfree : bool = false
 var progress : float
+@onready var gpu_particles_2d = $GPUParticles2D
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	gpu_particles_2d.emitting = true
 	
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	children = get_children()
-	for i in children:
-		if i is RigidBody2D:
-			i.set_collision_layer_value(4,true)
-			i.set_collision_mask_value(4,true)
-	progress = WALL_BROKEN.get_shader_parameter("progress")
-	match startfree:
-		true:
-			counter += explodespeed
-			WALL_BROKEN.set("shader_parameter/progress",counter)
-			print(progress)
-			if progress > 1.0:
-				queue_free()
-			
-		false:
-			pass
-	
-
-
-func _on_timer_timeout():
-	startfree = true
-	timer.start()
-	
+#
+## Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+	#children = get_children()
+	#for i in children:
+		#if i is RigidBody2D:
+			#i.set_collision_layer_value(4,true)
+			#i.set_collision_mask_value(4,true)
+	#progress = WALL_BROKEN.get_shader_parameter("progress")
+	#match startfree:
+		#true:
+			#counter += explodespeed
+			#WALL_BROKEN.set("shader_parameter/progress",counter)
+			#print(progress)
+			#if progress > 1.0:
+				#queue_free()
+			#
+		#false:
+			#pass
+	#
+#
+#
+#func _on_timer_timeout():
+	#startfree = true
+	#timer.start()
+	#
