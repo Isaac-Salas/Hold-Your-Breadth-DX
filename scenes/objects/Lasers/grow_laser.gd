a7d27935cba7663d01d24e9f40b83e63f7e78ebb
Alejandro Alonso Sánchez <a.alonsosanchez@ugto.mx>
2025-07-13 13:08:50 -0600

    Laserfix......

--- scenes/objects/Lasers/grow_laser.gd
+++ scenes/objects/Lasers/grow_laser.gd
@@ -8,29 +8,28 @@ var hit = Vector2(0,200)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	line.add_point(Vector2.ZERO)
+	line.add_point(raycast.target_position)
+	line.width = 3
+
 	if raycast.is_colliding():
 		var collider = raycast.get_collider()
 		if collider.is_in_group("button_actionable"):
 			collider.set_size(target_scale)
-	line.global_position = raycast.global_position
-	line.add_point(Vector2(0,0))
-	line.add_point(raycast.target_position)
-	line.width = 3
-	hitsprite.global_position = raycast.target_position
 
+		hitsprite.global_position = raycast.get_collision_point()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var collider = raycast.get_collider()
-	if raycast.is_colliding() and collider.global_position != hit:
-		hit = collider.global_position
-		line.set_point_position(1,Vector2(raycast.get_collision_point().distance_to(line.global_position),0))
-		line.width = 3
-		hitsprite.global_position = raycast.get_collision_point()
-		if collider.is_in_group("button_actionable") and !collider.is_in_group("Meatbox"):
-			collider.set_size(target_scale)
-			
-			#collider._on_ray_collided(target_size)
-			#GlobalVar.MinCap = target_size
-			#GlobalVar.MaxCap = target_size
-			#GlobalVar.sizefactor = target_size dante was here
+	if raycast.is_colliding():
+		var collider = raycast.get_collider()
+		var collision_point = raycast.get_collision_point()
+		
+		if collision_point.distance_to(hit) > 1.0:
+			hit = collision_point
+			line.set_point_position(1,Vector2(raycast.get_collision_point().distance_to(line.global_position),0))
+			line.width = 3
+			hitsprite.global_position = collision_point
+
+			if collider.is_in_group("button_actionable") and not collider.is_in_group("Meatbox"):
+				collider.set_size(target_scale)
